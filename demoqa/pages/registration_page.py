24c172910561e9639da48f780e7da6a13fb0cd6c
Elena Ter-Akopova <testfrog@yandex.ru>
2023-09-03 13:26:29 +0300

    убираем рекламу в форме регистрации

--- demoqa/pages/registration_page.py
+++ demoqa/pages/registration_page.py
@@ -9,6 +9,7 @@ class RegistrationPage:
     def open(self):
         browser.open('https://demoqa.com/automation-practice-form/')
         browser.element('footer').execute_script('element.remove()')
+        browser.element('#fixedban').execute_script('element.remove()')
 
     def fill_first_name(self, value):
         browser.element('#firstName').should(be.blank).type(value)
